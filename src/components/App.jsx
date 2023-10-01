081e634c1ea8b47303e93ff67842fef73d54c789
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-10-01 20:18:16 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,6 +22,7 @@ export const App = () => {
           <Route path="reviews" element={<Reviews />} />
         </Route>
       </Route>
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 };
